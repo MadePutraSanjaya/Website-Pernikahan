47c616ed68ba973ee7a0d578ad2d39148aad1f52
GusKertha <aguskerthaa@gmail.com>
2023-11-28 00:08:51 +0800

    Revert "membuat section 3 dan 4 tapi belum selesai"
    This reverts commit dae7800674bb9b4e74d26633e280d3881f2443cb.

--- script/script.js
+++ script/script.js
@@ -1,28 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Set the date we're counting down to
+
     var countDownDate = new Date("Jan 1, 2024 00:00:00").getTime();
   
-    // Update the countdown every 1 second
+   
     var x = setInterval(function () {
-      // Get the current date and time
+
       var now = new Date().getTime();
   
-      // Calculate the time remaining
+     
       var distance = countDownDate - now;
   
-      // Calculate days, hours, minutes, and seconds
+   
       var days = Math.floor(distance / (1000 * 60 * 60 * 24));
       var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
   
-      // Display the countdown
-      document.getElementById("days").innerHTML = days + "days ";
-      document.getElementById("hours").innerHTML = hours + "hours ";
-      document.getElementById("minutes").innerHTML = minutes + "minutes ";
-      document.getElementById("seconds").innerHTML = seconds + "seconds ";
+   
+      document.getElementById("days").innerHTML = "<div>" + days + "</div><div>days</div>";
+      document.getElementById("hours").innerHTML = "<div>" + hours + "</div><div>hours</div>";
+      document.getElementById("minutes").innerHTML = "<div>" + minutes + "</div><div>minutes</div>";
+      document.getElementById("seconds").innerHTML = "<div>" + seconds + "</div><div>seconds</div>";
+      
+      
   
-      // If the countdown is over, display a message
+    
       if (distance < 0) {
         clearInterval(x);
         document.getElementById("countdown").innerHTML = "EXPIRED";
