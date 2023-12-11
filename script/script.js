475ba2feb77c63f20901d236ae085328e991b688
MadePutraSanjaya <madeputra.6166@gmail.com>
2023-12-11 18:02:51 +0800

    Finishing Website Pernikahan

--- script/script.js
+++ script/script.js
@@ -1,34 +1,28 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
+  var countDownDate = new Date("Jan 1, 2024 00:00:00").getTime();
+  var x = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
 
-    var countDownDate = new Date("Jan 1, 2024 00:00:00").getTime();
-  
-   
-    var x = setInterval(function () {
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-      var now = new Date().getTime();
-  
-     
-      var distance = countDownDate - now;
-  
-   
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  
-   
-      document.getElementById("days").innerHTML = "<div>" + days + "</div><div>days</div>";
-      document.getElementById("hours").innerHTML = "<div>" + hours + "</div><div>hours</div>";
-      document.getElementById("minutes").innerHTML = "<div>" + minutes + "</div><div>minutes</div>";
-      document.getElementById("seconds").innerHTML = "<div>" + seconds + "</div><div>seconds</div>";
-      
-      
-  
-    
-      if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("countdown").innerHTML = "EXPIRED";
-      }
-    }, 1000);
-  });
-  
+    document.getElementById("days").innerHTML =
+      "<div>" + days + "</div><div>days</div>";
+    document.getElementById("hours").innerHTML =
+      "<div>" + hours + "</div><div>hours</div>";
+    document.getElementById("minutes").innerHTML =
+      "<div>" + minutes + "</div><div>minutes</div>";
+    document.getElementById("seconds").innerHTML =
+      "<div>" + seconds + "</div><div>seconds</div>";
+
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+});
